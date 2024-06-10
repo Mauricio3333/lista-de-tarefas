35f22c5a6bcbb61b5dbb62637220ff9f63dc9623
Mauricio Rocha Silva <mauricio_apista2@hotmail.com>
2024-06-09 22:25:12 -0300

    Meu primeiro projeto no Github

---
+++ README.md
@@ -0,0 +1,53 @@
+# Lista de Tarefas
+
+A Lista de Tarefas é uma aplicação simples que permite ao usuário adicionar, visualizar, marcar como concluída, excluir e pesquisar tarefas. É uma ferramenta útil para organizar tarefas diárias, projetos pessoais ou qualquer outra lista de atividades que precise ser gerenciada.
+
+## Funcionalidades
+
+- **Adicionar Tarefa:** Adicione novas tarefas à lista digitando o texto da tarefa e pressionando Enter ou clicando em um botão de adicionar.
+
+- **Hora e Data Da Tarefa Criada** Quando adicionar uma tarefa, amostrará a Hora e Data em que a tarefa foi criada.
+
+- **Marcar Tarefa como Realizada:** Marque uma tarefa como concluída clicando na caixa de seleção associada a ela. A tarefa será marcada com um estilo visual indicando que foi concluída.
+
+- **Excluir Tarefa:** Remova uma tarefa da lista clicando em um ícone de lixeira associado a ela.
+
+- **Deletar Todas as Tarefas:** Remova todas as tarefas da lista de uma vez, clicando em um botão para confirmar a ação.
+
+- **Filtrar Tarefas:** Filtrar as tarefas da lista com base no texto inserido em um campo de pesquisa. As tarefas que correspondem ao texto digitado serão exibidas, enquanto as outras serão ocultadas.
+
+## Como Usar
+
+1. **Adicionar Tarefa:**
+   - Digite o texto da nova tarefa no campo de entrada fornecido.
+   - Pressione Enter ou clique no botão de adicionar.
+
+2. **Marcar Tarefa como Realizada:**
+   - Clique na caixa de seleção ao lado da tarefa que deseja marcar como concluída.
+
+3. **Excluir Tarefa:**
+   - Clique no ícone de lixeira ao lado da tarefa que deseja excluir.
+
+4. **Deletar Todas as Tarefas:**
+   - Clique em um botão específico para deletar todas as tarefas da lista. Uma confirmação será solicitada antes da exclusão.
+
+5. **Filtrar Tarefas:**
+   - Digite o texto de busca no campo de pesquisa para filtrar as tarefas com base no texto inserido.
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS (SASS)
+- JavaScript
+
+## Contribuindo
+
+Contribuições são bem-vindas! Sinta-se à vontade para abrir uma issue ou enviar um pull request com melhorias, correções de bugs ou novas funcionalidades.
+
+## Autor
+
+[Mauricio Rocha Silva](https://github.com/Mauricio3333)
+
+## Licença
+
+Este projeto está licenciado sob a [MIT License](https://opensource.org/licenses/MIT).
---
+++ assets/js/app.js
@@ -0,0 +1,164 @@
+const inputAddTarefa = document.getElementById("inputAddTarefa");
+const listaDeTarefas = document.getElementById("listaDeTarefas");
+const inputSearch = document.querySelector(".pesquisar");
+const lupa = document.getElementById("lupa");
+const closeInput = document.getElementById("clearButton")
+
+// Função para carregar tarefas do localStorage
+const carregarTarefas = () => {
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    tarefas.forEach(tarefa => {
+        listaDeTarefas.appendChild(criarElementos(tarefa));
+    });
+};
+
+// Função para criar elementos de tarefa
+const criarElementos = ({ texto, realizado, dataHora }) => {
+    const li = document.createElement('li');
+    li.className = 'item';
+    li.setAttribute('data-datetime', dataHora);
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox';
+    checkbox.checked = realizado;
+    checkbox.addEventListener('click', marcarTarefaRealizada );
+
+    const spanTexto = document.createElement('span');
+    spanTexto.className = 'texto';
+    spanTexto.textContent = capitalizeFirstLetter(texto);
+    if (realizado) {
+        spanTexto.classList.add("marcarTexto");
+        li.classList.add("opc")
+    }
+
+    // const iconSpan = document.createElement('span');
+    // iconSpan.className = 'icon';
+
+    const icon = document.createElement('ion-icon');
+    icon.setAttribute("name", "close-outline");
+    icon.className = "remover" 
+    icon.addEventListener('click', excluirTarefa );
+
+    // iconSpan.appendChild(icon);
+
+    li.appendChild(checkbox);
+    li.appendChild(spanTexto);
+    // li.appendChild(iconSpan);
+    li.appendChild(icon)
+
+    return li;
+};
+
+// Função para adicionar tarefa
+const adicionarTarefa = () => {
+    const tarefaTexto = inputAddTarefa.value.trim();
+    if (!tarefaTexto) {
+        alert("Tarefa vazia, adicione algo!");
+        return;
+    }
+
+    if (verificarTarefaExistente(tarefaTexto)) {
+        alert("Essa tarefa já foi adicionada!");
+        return;
+    }
+
+    const tarefa = { texto: tarefaTexto, realizado: false, dataHora: obterDataHoraAtual() };
+    listaDeTarefas.appendChild(criarElementos(tarefa));
+    inputAddTarefa.value = '';
+    salvarTarefas();
+};
+
+// Função para verificar se a tarefa já existe
+const verificarTarefaExistente = (textoNovaTarefa) => {
+    return Array.from(document.querySelectorAll(".texto"))
+        .some(item => item.textContent.toLowerCase() === textoNovaTarefa.toLowerCase());
+};
+
+// Função para salvar tarefas no localStorage
+const salvarTarefas = () => {
+    const tarefas = Array.from(document.querySelectorAll(".item")).map(item => ({
+        texto: item.querySelector(".texto").textContent,
+        realizado: item.querySelector(".checkbox").checked,
+        dataHora: item.getAttribute("data-datetime")
+    }));
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+};
+
+// Função para marcar tarefa como realizada
+const marcarTarefaRealizada  = (e) => {
+    e.target.parentNode.classList.toggle("opc")
+    e.target.nextElementSibling.classList.toggle("marcarTexto")
+    salvarTarefas();
+};
+
+// Função para excluir tarefa
+const excluirTarefa  = (e) => {
+    e.target.closest('.item').remove();
+    salvarTarefas();
+};
+
+// Função para obter a data e hora atual
+const obterDataHoraAtual = () => {
+    return new Date().toLocaleString();
+};
+
+// Função para deletar todas as tarefas
+const deletarTudo = () => { 
+    if (listaDeTarefas.children.length > 0 && confirm("Tem certeza, que quer deletar tudo?")) {
+        listaDeTarefas.textContent = "";
+        esconderCampoPesquisa()
+        salvarTarefas();
+
+    }else if(listaDeTarefas.children.length <= 0){
+        alert("Não tem tarefas para excluir!");
+    }
+};
+
+// Função para filtrarTarefa tarefas
+const filtrarTarefa = () => {
+    const textoProcurado = inputSearch.value.toLowerCase();
+    document.querySelectorAll(".item").forEach(item => {
+        const textoItem = item.querySelector(".texto").textContent.toLowerCase();
+        item.style.display = textoItem.includes(textoProcurado) ? 'flex' : 'none';
+    });
+};
+
+// Função para mostrar o campo de pesquisa
+const mostrarCampoPesquisa = () => {
+    inputSearch.focus()
+    inputSearch.classList.add("aparecer");
+    closeInput.style.visibility = "visible"
+    closeInput.style.opacity = 1
+    lupa.style.transform = "scale(3.0)";
+    lupa.style.opacity = 0;
+    lupa.style.visibility = "hidden";
+    
+};
+
+// Função para esconder o campo de pesquisa
+const esconderCampoPesquisa = () => {
+    inputSearch.classList.remove("aparecer");
+    closeInput.style.visibility = "hidden"
+    closeInput.style.opacity = 0
+    inputSearch.value = "";
+    lupa.style.transform = "scale(1.0)";
+    lupa.style.visibility = "visible";
+    lupa.style.opacity = 1;
+    filtrarTarefa()
+};
+
+// Função para capitalizar a primeira letra de um texto
+const capitalizeFirstLetter = (texto) => {
+    return texto.charAt(0).toUpperCase() + texto.slice(1);
+};
+
+// Eventos
+inputAddTarefa.addEventListener("keydown", (e) => {
+    if (e.key === 'Enter') adicionarTarefa();
+});
+
+lupa.addEventListener("click", mostrarCampoPesquisa);
+inputSearch.addEventListener("input", filtrarTarefa);
+closeInput.addEventListener("click", esconderCampoPesquisa);
+window.addEventListener("load", carregarTarefas);
